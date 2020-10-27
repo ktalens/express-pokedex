00612698b22916e619c4adafb430239f68565f40
ktalens <ktal66@yahoo.com>
2020-10-27 01:52:06 -0700

    added delete route

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models')
-const axios = require('axios'); 
+const axios = require('axios');
+const methodOverride= require('method-override') //nmp i method-override
+
+router.use(methodOverride('_method')) // looks at any request object coming in, and if there's a querystring, it looks to..... 
 
 router.use(express.urlencoded({ extended: false }));
 
@@ -20,20 +23,19 @@ router.get('/', function(req, res) {
 });
 
 
-
-
-
 // POST /pokemon - receive the name of a pokemon and add it to the database
 router.post('/', function(req, res) {
   // TODO: Get form data and add a new record to DB
   let addFave = req.body.name
   db.pokemon
     .findOrCreate({
-      where: {name: addFave}
+      where: {
+        name: addFave}
     })
     .then(([createdFave,wasCreated])=>{
       console.log('created: ',createdFave)
       res.redirect('/')
+      
     })
     .catch(err=>{
       console.log('Error: ', err)
@@ -52,6 +54,22 @@ router.get('/:idx',(req,res)=>{
   .catch(err=>{
     console.log(err)//res.render('show')
   })
+})
+
+router.delete('/:idx',(req,res)=>{
+  let unFavorite = req.body.name
+  //res.send(unFavorite)
+  db.pokemon.destroy({
+    where: {name: unFavorite}
+  })
+  .then(numRowsDeleted=>{
+    console.log(numRowsDeleted)
+    res.redirect('/pokemon')
+  })
+  .catch(err=>{
+    console.log(err)
+  })
+  
 })
 
 module.exports = router;
